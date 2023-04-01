e6518d4552f00e320b50a1e14ad7e4e818c329c7
Natura7 <RomanVydysh7@gmail.com>
2023-04-01 18:36:56 +0300

    lesson 30

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,30 +2,15 @@ import React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-
-const postData = [
-	{ id: 1, message: 'Hello', like: 5 },
-	{ id: 2, message: 'I know React', like: 3 },
-	{ id: 3, message: 'First comment', like: 0 },
-]
-let dialogsData = [
-	{ name: 'Roman', id: 1 },
-	{ name: 'Andriy', id: 2 },
-	{ name: 'Diana', id: 3 },
-	{ name: 'Yulia', id: 4 },
-	{ name: 'Stepan', id: 5 },
-]
-let messageData = [
-	{ message: 'hi', id: 1 },
-	{ message: 'how are you?', id: 2 },
-	{ message: 'I`m fine', id: 3 },
-	{ message: 'Hello', id: 4 },
-	{ message: 'bye', id: 5 },
-]
+import state from './redux/state'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<App posts={postData} dialogs={dialogsData} message={messageData} />
+		<App
+			posts={state.postData}
+			dialogs={state.dialogsData}
+			message={state.messageData}
+		/>
 	</React.StrictMode>
 )
---
+++ my-app/src/redux/state.jsx
@@ -0,0 +1,23 @@
+const state = {
+	postData: [
+		{ id: 1, message: 'Hello', like: 5 },
+		{ id: 2, message: 'I know React', like: 3 },
+		{ id: 3, message: 'First comment', like: 0 },
+	],
+	dialogsData: [
+		{ name: 'Roman', id: 1 },
+		{ name: 'Andriy', id: 2 },
+		{ name: 'Diana', id: 3 },
+		{ name: 'Yulia', id: 4 },
+		{ name: 'Stepan', id: 5 },
+	],
+	messageData: [
+		{ message: 'hi', id: 1 },
+		{ message: 'how are you?', id: 2 },
+		{ message: 'I`m fine', id: 3 },
+		{ message: 'Hello', id: 4 },
+		{ message: 'bye', id: 5 },
+	],
+}
+
+export default state
